b90caaa881e49770baf8cb8a6ec90fb0520bbd41
Kazuma <kazuma.work77@gmail.com>
2025-04-06 03:28:03 +0800

    fix: display

--- packages/client/app/layout.tsx
+++ packages/client/app/layout.tsx
@@ -1,53 +1,53 @@
-import { Inter } from "next/font/google";
-import type React from "react";
-import "./globals.css";
-import NextAuthProvider from "@/components/next-auth-provider";
-import { ThemeProvider } from "@/components/theme-provider";
-import dynamic from "next/dynamic";
-import { Toaster } from "sonner";
+import { Inter } from 'next/font/google';
+import type React from 'react';
+import './globals.css';
+import NextAuthProvider from '@/components/next-auth-provider';
+import { ThemeProvider } from '@/components/theme-provider';
+import dynamic from 'next/dynamic';
+import { Toaster } from 'sonner';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-	title: "WorldVisionAI",
-	description: "",
+  title: 'WorldVisionAI',
+  description: '',
 };
 
 const MiniKitProviderWithNoSSR = dynamic(
-	() => import("../components/minikit-provider"),
-	{ ssr: false },
+  () => import('../components/minikit-provider'),
+  { ssr: false },
 );
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	// const ErudaProvider = dynamic(
-	//   () => import("../components/Eruda").then((c) => c.ErudaProvider),
-	//   {
-	//     ssr: false,
-	//   }
-	// );
-	return (
-		<html lang="ja" suppressHydrationWarning>
-			<body className={inter.className}>
-				<NextAuthProvider>
-					{/* <ErudaProvider> */}
-					<MiniKitProviderWithNoSSR>
-						<ThemeProvider
-							attribute="class"
-							defaultTheme="light"
-							enableSystem
-							disableTransitionOnChange
-						>
-							{children}
-							<Toaster />
-						</ThemeProvider>
-					</MiniKitProviderWithNoSSR>
-					{/* </ErudaProvider> */}
-				</NextAuthProvider>
-			</body>
-		</html>
-	);
+  const ErudaProvider = dynamic(
+    () => import('../components/Eruda').then((c) => c.ErudaProvider),
+    {
+      ssr: false,
+    },
+  );
+  return (
+    <html lang="ja" suppressHydrationWarning>
+      <body className={inter.className}>
+        <NextAuthProvider>
+          <ErudaProvider>
+            <MiniKitProviderWithNoSSR>
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="light"
+                enableSystem
+                disableTransitionOnChange
+              >
+                {children}
+                <Toaster />
+              </ThemeProvider>
+            </MiniKitProviderWithNoSSR>
+          </ErudaProvider>
+        </NextAuthProvider>
+      </body>
+    </html>
+  );
 }
