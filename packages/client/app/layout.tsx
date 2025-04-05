6ad30ef742669692980fb0f518494ac1d17d93b1
Kazuma <kazuma.work77@gmail.com>
2025-04-06 03:42:11 +0800

    fix: refactoring

--- packages/client/app/layout.tsx
+++ packages/client/app/layout.tsx
@@ -23,29 +23,29 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const ErudaProvider = dynamic(
-    () => import('../components/Eruda').then((c) => c.ErudaProvider),
-    {
-      ssr: false,
-    },
-  );
+  //   const ErudaProvider = dynamic(
+  //     () => import('../components/Eruda').then((c) => c.ErudaProvider),
+  //     {
+  //       ssr: false,
+  //     },
+  //   );
   return (
     <html lang="ja" suppressHydrationWarning>
       <body className={inter.className}>
         <NextAuthProvider>
-          <ErudaProvider>
-            <MiniKitProviderWithNoSSR>
-              <ThemeProvider
-                attribute="class"
-                defaultTheme="light"
-                enableSystem
-                disableTransitionOnChange
-              >
-                {children}
-                <Toaster />
-              </ThemeProvider>
-            </MiniKitProviderWithNoSSR>
-          </ErudaProvider>
+          {/* <ErudaProvider> */}
+          <MiniKitProviderWithNoSSR>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="light"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </MiniKitProviderWithNoSSR>
+          {/* </ErudaProvider> */}
         </NextAuthProvider>
       </body>
     </html>
